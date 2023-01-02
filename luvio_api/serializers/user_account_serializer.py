30080576a78197fc060b5a662e1dc0bec634dafe
Long Nguyen <nguyenhailong253@gmail.com>
2023-01-02 16:17:23 +1100

    add update serializer for useraccount

--- luvio_api/serializers/user_account_serializer.py
+++ luvio_api/serializers/user_account_serializer.py
@@ -15,3 +15,13 @@ class UserAccountSerializer(serializers.ModelSerializer):
         user.set_password(raw_password)
         user.save()
         return user
+
+    def update(self, instance: UserAccount, validated_data: dict) -> UserAccount:
+        instance.email = validated_data.get("email", instance.email)
+        instance.username = validated_data.get("username", instance.username)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.date_of_birth = validated_data.get("date_of_birth", None)
+        instance.mobile = validated_data.get("mobile", None)
+        instance.save()
+        return instance
